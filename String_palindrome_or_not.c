84ff3e8a249e045f816f184390be5f0d1d33d4d9
Max29087 <topusorker087@gmai.com>
2024-04-23 21:43:27 +0600

    add new file

--- String_palindrome_or_not.c
+++ String_palindrome_or_not.c
@@ -7,11 +7,12 @@ int main()
     fgets(str1, 1000, stdin);
     int len = strlen(str1);
     int i, j;
-    for (j = 0, i = len - 1; i >= 0;i--)
+    for (j = 0, i = len - 2; i >= 0;i--,j++)
     {
         str2[j] = str1[i];
     }
-    str2[j] = '\0';
+    str2[j] = '\n';//fgets function take newline(\n)as a input.So we need to initialize a newline on str2;
+    str2[j+1] = '\0';
     int x = strcmp(str1, str2);
     if (x == 0)
     {
