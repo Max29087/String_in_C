211076eaa92c2f471e32750d57d9db309cd61242
Max29087 <topusorker087@gmai.com>
2024-05-05 23:19:26 +0600

    add new file

--- add-two_string_without_function.c
+++ add-two_string_without_function.c
@@ -5,7 +5,9 @@ int main()
     char str1[1000];
     char str2[1000];
     fgets(str1,1000,stdin);
+    str1[strcspn(str1,"\n")]='\0';
     fgets(str2,1000,stdin);
+    str2[strcspn(str2,"\n")]='\0';
     int len = strlen(str1);
     int j=0;
     while(str2[j]!='\0')
--- string_reverse_without_function.c
+++ string_reverse_without_function.c
@@ -5,6 +5,7 @@ int main()
     char str1[1000];
     char str2[1000];
     fgets(str1, 1000, stdin);
+    str1[strcspn(str1,"\n")]='\0';
     int len = strlen(str1);
     int i, j;
     for (j = 0, i = len - 1; i >= 0; j++, i--)
